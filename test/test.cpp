4d06ad89d56f7e35ed4f6a6b8bdd705bb707b31a
Горлов Максим <gorlov.maxim@gmail.com>
2021-04-30 20:10:39 +0300

    repeat test

--- test/test.cpp
+++ test/test.cpp
@@ -17,6 +17,16 @@ int main(void)
   LL_LOG_NFO(cppt::string::replace_all("apple", "p", "_foo_"));
   LL_LOG_NFO(cppt::string::rand_alnum_str(50));
 
+  int repeatCount = 111;
+  std::string repeatSource = "-=[|]=-";
+  std::string repeated = cppt::string::repeat(repeatSource, repeatCount);
+  LL_LOG_NFO(repeated);
+  LL_LOG_NFO("Repeating. String: " << repeatSource <<
+             "; src length: " << repeatSource.length() <<
+             "; result length: " << repeated.length() <<
+             "; count: " << repeatCount <<
+             "; ratio: " << (repeated.length() / repeatCount / repeatSource.length()));
+
   cppt::user::CUser user;
   LL_LOG_NFO("User name: " << user.username() << " with uid:gid " << user.userID() << ":" << user.groupID() << " and home in " << user.home());
 
